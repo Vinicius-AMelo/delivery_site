15fed7dc9ebe2ea8e95a878c6e5a7fc6bacd646b
Vinicius-AMelo <vinicius.apol.melo@gmail.com>
2022-08-18 15:37:27 -0300

    Redirecionamento pra página principal após login bem sucedido

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,8 @@ import styles from '../scss/home.module.scss'
 function Home() {
   return (
     <div className={styles.home}>
-      <h1>a</h1>
+      <h1>PÁGINA EM CONSTRUÇÃO</h1>
+      <p>Volte aqui em alguns dias!</p>
     </div>
   )
 }
